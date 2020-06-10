40bd9baa7b44b6aa34058084dfb501a39bf0285a
Cronian <Cronian89@gmail.com>
2020-06-10 13:06:52 +0900

    Update Cola.java

--- strategy/Cola.java
+++ strategy/Cola.java
@@ -1,8 +1,10 @@
 public Class Cola{
     private BrandStrategy brandStrategy;
-    private TasteStrategy tasteStratege;
     public void product() {
       brandStrategy.brand();
-      tasteStratege.brand();
+    }
+    
+    public void setBrand(BrandStrategy brand) {
+        this.brandStrategy = brand;
     }
 }
