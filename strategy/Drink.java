494411c59802039c8c5116032e9a31113e7f0f9e
Cronian <Cronian89@gmail.com>
2020-06-10 21:01:33 +0900

    Create Drink.java

---
+++ strategy/Drink.java
@@ -0,0 +1,12 @@
+public class Drink {
+  public static void main(String args[]) {
+    Cola cocaCola = new CocaCola();
+    Cola pepsi = new Pepsi();
+    
+    cocaCola.setBrandStrategy(new CocaBrand());
+    pepsi.setBrandStrategy(new PepsiBrand());
+    
+    cocaCola.brand();
+    pepsi.brand();
+  }
+}
