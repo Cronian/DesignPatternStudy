5a56b5a69e2b99d10e998705cd2e6284b575a668
Cronian <Cronian89@gmail.com>
2020-06-10 17:49:19 +0900

    Update CocaBrand.java

--- strategy/CocaBrand.java
+++ strategy/CocaBrand.java
@@ -1,3 +1,10 @@
 public class CocaBrand implements BrandStrategy {
-  
+    private String brand;
+    public Coca() {
+      this.brand = "Coca Cola";
+    }
+    
+    public void brand() {
+      System.out.println(this.brand); 
+    }
 }
